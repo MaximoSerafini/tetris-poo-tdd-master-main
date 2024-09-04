f4c91a6a3f3afe5955d6307b46d5fe0c90c341e1
DiMaxi <maximoserafini44@gmail.com>
2024-09-04 19:02:04 -0300

    2testgiro

--- src/test/java/com/tetris/TestTetris2.java
+++ src/test/java/com/tetris/TestTetris2.java
@@ -5,10 +5,10 @@ import org.junit.Test;
 
 public class TestTetris2 {
     @Test
-    public void girar_palo() {
+    public void girar_palo_izq() {
         PieceStick pieceStick = new PieceStick();
 
-        pieceStick.rotate();
+        pieceStick.rotateIzq();
 
         int[][] formaEsperada = {
             {0, 0, 0, 0},
@@ -21,5 +21,24 @@ public class TestTetris2 {
 
         assertArrayEquals(formaEsperada, formaActual);
 
-    }      
+    }
+
+    @Test
+    public void girar_palo_Der() {
+        PieceStick pieceStick = new PieceStick();
+
+        pieceStick.rotateDer();
+
+        int[][] formaEsperada = {
+            {1, 1, 1, 1},
+            {0, 0, 0, 0},
+            {0, 0, 0, 0},
+            {0, 0, 0, 0}
+        };
+
+        int[][] formaActual = pieceStick.getShape();
+
+        assertArrayEquals(formaEsperada, formaActual);
+
+    } 
 }
