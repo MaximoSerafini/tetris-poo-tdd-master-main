f33a6d04bdd20266b62a86f5f98fee47ab4a1eee
Santiago <sant.serrano@icloud.com>
2024-09-16 17:14:11 -0300

    test 5

--- src/test/java/com/tetris/TestTetris5.java
+++ src/test/java/com/tetris/TestTetris5.java
@@ -1,5 +1,44 @@
 package com.tetris;
+import org.junit.Test;
 
 public class TestTetris5 {
+
+
+    @Test
+    public void testLines() {
+        
+        Board board = new Board();
+        PieceStick piece1 = new PieceStick();
+        board.addPieceBoard(piece1);
+        piece1.moveDown(); //???
     
+        // deberia de incluir chequear el final del juego
+        board.updateBoard();
+    
+    }
+
+    @Test
+    public void testIsLineCompleteTrue() {
+        
+    }
+
+    @Test
+    public void testRemoveLine() {
+        
+    }
+
+    @Test
+    public void testClearLinea() {
+        
+    }
+
+    @Test
+    public void testCheckFinalDelJuego() {
+        
+    }
+
+    
+
+
+
 }
